7beae27f9cb7d158cfd9ab95e05e3b1dd657eb58
LiXiang <xianglqqo@gmail.com>
2018-10-23 13:17:13 +0800

    更新ioa至4.4.0

--- app/router.js
+++ app/router.js
@@ -1,26 +1,26 @@
 'use strict';
 
-let app = require('ioa')
+let { router, middleware } = require('ioa')
 
-let { token } = app.middleware
+let { token } = middleware
 
-app.get('/', 'index.home')
+router.get('/', 'index.home')
 
-app.post('/login', 'index.login')
+router.post('/login', 'index.login')
 
-app.get('/news', 'news.home')
+router.get('/news', 'news.home')
 
-app.get('/news/:id/details/:kk', 'news.details')
+router.get('/news/:id/details/:kk', 'news.details')
 
-app.get('/sms/:id/sd/:kk', 'index.sms')
+router.get('/sms/:id/sd/:kk', 'index.sms')
 
-app.post('/sms/:id/sd/:kk', 'index.sms')
+router.post('/sms/:id/sd/:kk', 'index.sms')
 
-app.get('/admin', 'admin.index.details')
+router.get('/admin', 'admin.index.details')
 
 ////////// REST路由 ////////////
 
 // 模糊匹配路由，泛解析，通用模型控制器
-app.resources('/rest/:name', 'rest')
+router.resources('/rest/:name', 'rest')
 
-// app.get('/rest/123', 'rest.123')
+// router.get('/rest/123', 'rest.123')
